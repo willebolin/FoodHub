10ec075612ecec89e6af8d374414fb6aab641a37
AntoineLrt <56179864+AntoineLrt@users.noreply.github.com>
2020-05-13 13:50:16 -0700

    Update ramenSchool.js

--- src/Backend/ramenSchool.js
+++ src/Backend/ramenSchool.js
@@ -3,7 +3,7 @@ var e1 = {
     series_title: "Ramen School",
     episode_title: 'How to Make Basic Ramen Broth',
     episode_number: '1',
-    description: 'In this new series we'll be going through how to make great ramen at home. Be warned, it's not a short process but I wanted to show you the theory behind ramen so that you can produce your own, authentic and unique ramen rather than just following recipes. Hopefully through these videos you'll understand the basics of what makes a great bowl of ramen.',
+    description: 'In this new series we\'ll be going through how to make great ramen at home. Be warned, it\'s not a short process but I wanted to show you the theory behind ramen so that you can produce your own, authentic and unique ramen rather than just following recipes. Hopefully through these videos you\'ll understand the basics of what makes a great bowl of ramen.',
 };
 
 var e2 = {
@@ -11,16 +11,65 @@ var e2 = {
     series_title: "Ramen School",
     episode_title: 'Three Styles of Chashu for Ramen',
     episode_number: '2',
-    description: 'We're continuing our Ramen School series and moving on to toppings. The most popular topping for ramen is "chashu", thinly sliced sweet soy-braised pork. ',
+    description: 'We\'re continuing our Ramen School series and moving on to toppings. The most popular topping for ramen is "chashu", thinly sliced sweet soy-braised pork. ',
+};
+
+var e3 = {
+    ID: 'F9IepMO2DIc',
+    series_title: "Ramen School",
+    episode_title: 'How to Make Ramen Eggs',
+    episode_number: '3',
+    description: 'The key to ramen eggs (known in Japanese as ajitsuke tamago) is that they aren\'t just flavoured by their marinade. The salty and sweet marinade actually acts as a cure to firm the whites and yolks, and give the yolks a savoury and jammy taste and consistency, which is a much better texture for ramen. Two days curing is about right for curing ramen eggs, but you can go more or less depending on the levels of salt and sugar in the liquid.',
+};
+
+var e4 = {
+    ID: 'V4iVfedppEs',
+    series_title: "Ramen School",
+    episode_title: 'How to Make Afuri-Style Yuzu Shio Ramen',
+    episode_number: '4',
+    description: 'Afuri is one of Tokyo\'s most famous ramen restaurants. Their signature ramen is the a Yuzu Shio style of ramen. This isn\'t intended to be a copycat recipe, but I think it\'s useful to have a guide to work toward when experimenting with ramen, to see how the process of making each of the elements of ramen can lead you toward a specific result.',
 };
 
 
-var ItsAlive1 = {
-    title: 'Just A Dash',
+var e5 = {
+    ID: '231z7uDBh9Q',
+    series_title: "Ramen School",
+    episode_title: 'Shoyu Ramen in 10 Minutes from Instant Noodles',
+    episode_number: '5',
+    description: 'If you aren\'t sure you want to spend hours (days!) making shoyu ramen from scratch, here\'s a recipe using instant noodles that is ready in just 10 minutes. It includes all of the 5 basic elements of ramen (Broth, Noodles, Tare, Oil and Toppings) and is an excellent way to upscale your instant noodles into a proper ramen fix. This video is brought to you by Maggi\'s Fusian Teriyaki Soupy Noodle.',
+};
+
+var e6 = {
+    ID: 'zM52pKvBCU0',
+    series_title: "Ramen School",
+    episode_title: 'Rich Double Soup Base for Ramen',
+    episode_number: '6',
+    description: 'In the first Ramen School video we went through the process of making a very basic ramen soup base using the "double soup" method. This time we\'re going to use the same method, but ramping things up a bit for a more complex soup base. Using different ingredients requires a slightly different process, and of course a different length of time to get the best out of them.',
+};
+
+var e7 = {
+    ID: '0Dv7zfWS3EQ',
+    series_title: "Ramen School",
+    episode_title: 'How to Make Garlic Shoyu Ramen',
+    episode_number: '7',
+    description: 'Shoyu ramen is one of the 4 basic ramen styles (along with shio, tonkotsu and miso) It’s a Tokyo style of ramen that uses soy sauce as the salty and umami base of the tare. Here’s how to make it.',
+};
+
+var e8 = {
+    ID: 'F-TQjevcOf4',
+    series_title: "Ramen School",
+    episode_title: 'How to Make Tonkotsu Ramen',
+    episode_number: '8',
+    description: 'Tonkotsu (pork bone) ramen is a labour of love. It can take 3 or 4 days to make - although most of that is just watching a pot boil - and you might wonder why anyone would ever go to the effort. ',
+};
+
+
+var ramenSchool = {
+    title: 'Ramen School',
     episode: [e1, e2, e3, e4, e5, e6, e7, e8],
-    description: "Join Bon Appètit test kitchen manager, Brad Lenoe, on a wild, roundabout and marginally scientific adventure exploring fermented foods and more. From cultured butter and kombucha, to kimchi and miso, to beer and tepache, learn how to make fermented and live foods yourself",
+    description: "In this new series we'll be going through how to make great ramen at home. Be warned, it's not a short process but I wanted to show you the theory behind ramen so that you can produce your own, authentic and unique ramen rather than just following recipes. Hopefully through these videos you'll understand the basics of what makes a great bowl of ramen. ",
     image: require('../ThumbNails/RamenSchool.jpg'),
     thumbnail: require('../ThumbNails/RamenSchoolthumb.png')
 };
 
-export var RamenSchool;
+export var ramenSchool;
