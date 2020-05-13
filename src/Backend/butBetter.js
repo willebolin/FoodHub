6a857e3d83f156f4e94cba13129a76e8c9f07b2e
AntoineLrt <56179864+AntoineLrt@users.noreply.github.com>
2020-05-13 11:31:46 -0700

    Update butBetter.js

--- src/Backend/butBetter.js
+++ src/Backend/butBetter.js
@@ -3,7 +3,7 @@ var e1 = {
   series_title: 'At Home but Better',
   episode_title: 'Making The Popeyes Chicken Sandwich At Home, But Better',
   episode_number: '1',
-  description: 'This is episode one',
+  description: 'What better way to celebrate 500,000 subscribers than making a fried chicken sandwich that rivals the Popeyes chicken sandwich. Buttermilk fried chicken, layered between a Japanese milk bun, black garlic mayo, homemade pickles. Its perfect. Making a chicken sandwich isnt just some recipe, its a technique, an art form if you will. Hope you guys get a kick out of this and make your very own sandwich!',
 };
 
 var e2 = {
@@ -11,7 +11,7 @@ var e2 = {
   series_title: 'At Home but Better',
   episode_title: 'Making Arbys Curly Fries at Home | But Better',
   episode_number: '2',
-  description: 'This is episode two',
+  description: 'Looks like the secret is out. You can make your own curly fries at home, and with super minimal ingredients and time. Once you hold the power of the curly, you hold the power of all. Welcome to our debut first episode of But Better. ',
 };
 
 var e3 = {
@@ -19,7 +19,7 @@ var e3 = {
   series_title: 'At Home but Better',
   episode_title: 'Making Chick-Fil-A Nuggets at Home | But Better',
   episode_number: '3',
-  description: 'This is episode three',
+  description: 'I promise you can have Chick Fil A Nuggets accompanied by the coveted chick fil a sauce made entirely homemade. Just get your hands on some MSG and you might be on your way to the best chicken nuggets you\'ll ever make in your life. Welcome back to the technical Episode 3 of But Better.',
 };
 
 var e4 = {
@@ -27,7 +27,7 @@ var e4 = {
   series_title: 'At Home but Better',
   episode_title: 'How To Out Pizza The Hut (and Dominos) | But Better',
   episode_number: '4',
-  description: 'This is episode four',
+  description: 'Making homemade pizza is easy. It\'s a different story when you decide to go head to head with Dominos and Pizza Hut to see who the true reigning king is. I made my decision, now you decide. Welcome back to But Better Episode 4. ',
 };
 
 var e5 = {
@@ -35,7 +35,7 @@ var e5 = {
   series_title: 'At Home but Better',
   episode_title: 'Making In-N-Out Burgers At Home | But Better',
   episode_number: '5',
-  description: 'This is episode five',
+  description: 'Starting episode 5 of But Better off strong with the best fast food burger In-N-Out at but of course.. better. We pulled out all the stops with homemade buns, the best burger sauce, griddled onions, you know... the works. ',
 };
 
 var e6 = {
@@ -43,7 +43,7 @@ var e6 = {
   series_title: 'At Home but Better',
   episode_title: 'Making The Taco Bell Crunchwrap Supreme At Home | But Better',
   episode_number: '6',
-  description: 'This is episode six',
+  description: 'Next up for But Better Ep. 6 we are tackling the heavy clouted taco bell chicken crunchwrap supreme. Can we survive this one? Yes. Yes we can. ',
 };
 
 var e7 = {
@@ -51,7 +51,7 @@ var e7 = {
   series_title: 'At Home but Better',
   episode_title: 'Making Panda Express Orange Chicken At Home | But Better',
   episode_number: '7',
-  description: 'This is episode seven',
+  description: 'Fried chicken, coated in a sweet soy orange glaze. Sounds easy enough to me... But Can we beat Panda Express? Welcome back for Episode 7. of But Better. ',
 };
 
 var e8 = {
@@ -59,7 +59,7 @@ var e8 = {
   series_title: 'At Home but Better',
   episode_title: 'Making McDonalds Chicken McNuggets At Home | But Better',
   episode_number: '8',
-  description: 'This is episode eight',
+  description: 'The coveted chicken McNugget is no secret. It\'s all in the technique and as it turns out, that\'s pretty easy too! And with that, welcome to episode 8. of But Better. :)',
 };
 
 
