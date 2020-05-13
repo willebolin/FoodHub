e7897a88719b49408723f3b0decf8fdbc73091df
AntoineLrt <56179864+AntoineLrt@users.noreply.github.com>
2020-05-13 13:35:04 -0700

    Create ramenSchool.js

---
+++ src/Backend/ramenSchool.js
@@ -0,0 +1,26 @@
+var e1 = {
+    ID: 'nscTA7QxryM',
+    series_title: "Ramen School",
+    episode_title: 'How to Make Basic Ramen Broth',
+    episode_number: '1',
+    description: 'In this new series we'll be going through how to make great ramen at home. Be warned, it's not a short process but I wanted to show you the theory behind ramen so that you can produce your own, authentic and unique ramen rather than just following recipes. Hopefully through these videos you'll understand the basics of what makes a great bowl of ramen.',
+};
+
+var e2 = {
+    ID: '83zf7vHiXUo',
+    series_title: "Ramen School",
+    episode_title: 'Three Styles of Chashu for Ramen',
+    episode_number: '2',
+    description: 'We're continuing our Ramen School series and moving on to toppings. The most popular topping for ramen is "chashu", thinly sliced sweet soy-braised pork. ',
+};
+
+
+var ItsAlive1 = {
+    title: 'Just A Dash',
+    episode: [e1, e2, e3, e4, e5, e6, e7, e8],
+    description: "Join Bon Appètit test kitchen manager, Brad Lenoe, on a wild, roundabout and marginally scientific adventure exploring fermented foods and more. From cultured butter and kombucha, to kimchi and miso, to beer and tepache, learn how to make fermented and live foods yourself",
+    image: require('../ThumbNails/ItsAlive1.jpg'),
+    thumbnail: require('../ThumbNails/ItsAlive1thumb.png')
+};
+
+export var ItsAlive1;
