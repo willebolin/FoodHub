f6d2e8aa1770da21b87bcc2c99c29a03fe244c11
AntoineLrt <56179864+AntoineLrt@users.noreply.github.com>
2020-05-14 22:03:59 -0700

    Create andyExplores.js

---
+++ src/Backend/andyExplores.js
@@ -0,0 +1,42 @@
+var e1 = {
+    ID: 'cT18Mrh5Gos',
+    series_title: "Andy Explores",
+    episode_title: 'Andy Learns Thai Cooking Techniques from a Thai Chef',
+    episode_number: '1',
+    description: 'Andy Baraghani loves Thai food, especially the flavors of the northeastern region of Isan. Fish sauce, lime, chilis amongst others make the cuisine distinct, but what does it take to cook Isan Thai food? We sent Andy to Somtum Der in New York's East Village to explore the pillars of Thai cooking. Join him to learn about Isan spices, how to make perfect Thai fried chicken, coconut sticky rice and so much more.',
+};
+
+var e2 = {
+    ID: 'EGaf6yMtBF0',
+    series_title: "Andy Explores",
+    episode_title: 'Andy Learns How to Cook Palestinian Food',
+    episode_number: '2',
+    description: 'Andy Baraghani is on to his next exploration and this time it's Palestinian cooking. He's joined by Rawia Bishara, a fixture in Palestinian cooking, to introduce him to the essentials of her culinary heritage at her restaurant Tanoreen. She teaches Andy how to cook three dishes: fatoush, makloobeh and knafeh.',
+};
+
+var e3 = {
+    ID: 'MSgbPZOIBHk',
+    series_title: "Andy Explores",
+    episode_title: 'Andy Learns How to Cook Senegalese Food',
+    episode_number: '3',
+    description: 'Andy Baraghani is basically starting from scratch with it comes to Senegalese food -- it's not a cuisine he is very familiar with. Join Andy on yet another culinary exploration as he learns how to make four traditional Senegalese dishes under the guidance of Pierre Thiam, the co-founder and executive chef at Teranga.',
+};
+
+var e4 = {
+    ID: 'lTUr-zU6AlA',
+    series_title: "Andy Explores",
+    episode_title: 'Andy Learns How to Cook Sichuan Food',
+    episode_number: '4',
+    description: 'Andy Baraghani loves Chinese food, so he was pretty excited when we challenged him to go learn the ins and outs of Sichuan cooking. Join Andy on a culinary exploration as he learns how to make four traditional Sichuan dishes under the guidance of Chinese food experience guide Liz Mo and Málà Project chef Qilong Zhao.',
+};
+
+
+var andyExplores = {
+    title: 'Andy Explores',
+    episode: [e1, e2, e3, e4],
+    description: "Andy Baraghani is inquisitive without being condescending. He's knowledgeable and doesn't talk over the experts. He's down to try anything without making faces at food he's unfamiliar with.",
+    image: require('../ThumbNails/andyExplores.jpg'),
+    thumbnail: require('../ThumbNails/andyExploresthumb.png')
+};
+
+export var andyExplores;
