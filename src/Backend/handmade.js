d3fb391be7898652665ad0f50f27810b79789944
AntoineLrt <56179864+AntoineLrt@users.noreply.github.com>
2020-05-14 22:53:24 -0700

    Create handmade.js

---
+++ src/Backend/handmade.js
@@ -0,0 +1,66 @@
+var e1 = {
+    ID: 'Jl0IDXkYKbk',
+    series_title: "Handmade",
+    episode_title: 'How Miami’s Chocolate Master Creates the Perfect Bar of Chocolate',
+    episode_number: '1',
+    description: 'On this episode of ‘Handmade,’ master chocolate maker Carolina Quijano walks us through her chocolate-making process, from hand selecting the best cocoa beans in the world to the roasting process to grinding, sifting, molding, and more. The result is pure and delicious chocolate from her shop Exquisito Chocolates in Miami.',
+};
+
+var e2 = {
+    ID: 'mTztlpAcips',
+    series_title: "Handmade",
+    episode_title: 'How a Former Rocket Scientist Makes the Best Copper Pots in America',
+    episode_number: '2',
+    description: 'Rocket scientist-turned-coppersmith Jim Hamann’s devotion to cookware came from a beautiful vintage pot he found on a trip to France. After starting a business restoring copper pots and pans for others, he decided to begin handcrafting his own, honoring the methods and quality of the centuries-old tradition at his company Duparquet Copper Cookware.',
+};
+
+var e3 = {
+    ID: '1EPDJBjZPJI',
+    series_title: "Handmade",
+    episode_title: 'How Tamales Are Made at One of NYC's Favorite Puebla Tamal Shops',
+    episode_number: '3',
+    description: 'On this episode of Handmade, Factory Tamal owner Fernando Lopez walks us through the complex and traditional process of hand-making tamales the way his family has done for generations.',
+};
+
+var e4 = {
+    ID: 'b5dv_j9SdC0',
+    series_title: "Handmade",
+    episode_title: 'The Best Slice in NYC Comes From Scarr's Pizza's House-Milled Flour Operation',
+    episode_number: '4',
+    description: 'On this episode of Handmade, Scarr's Pizza owner Scarr Pimentel shows us why his fresh-milled wheat makes his pizza slices the healthiest, and tastiest in NYC.',
+};
+
+var e5 = {
+    ID: 'zQQQOaTwrI8',
+    series_title: "Handmade",
+    episode_title: 'The Hand-Ripped Noodle Empire That Took Over NYC',
+    episode_number: '5',
+    description: 'On this episode of Handmade, Xi'an Famous foods CEO Jason Wang shows us what goes into hand-ripping three to five thousand noodle dishes a day at the family owned, fast-casual New York City-based sensation.',
+};
+
+var e6 = {
+    ID: 'H1QYeuy3buE',
+    series_title: "Handmade",
+    episode_title: 'How a Ceramics Master Makes Plates for Michelin-Starred Restaurants',
+    episode_number: '6',
+    description: 'On this episode of Handmade, ceramicist Jono Pandolfi shows us how his team makes 300 handcrafted plates, mugs, cups, and other dinnerware for some of America’s best restaurants.',
+};
+
+var e7 = {
+    ID: 'AZDHeOeMR3g',
+    series_title: "Handmade",
+    episode_title: 'How Knives Are Made for New York's Best Restaurants',
+    episode_number: '7',
+    description: 'Handmade takes you behind the scenes to see how Will Griffin of Griffin Bladeworks in New York City hand-forges knives for some of New York City's best Restaurants.',
+};
+
+
+var handmade = {
+    title: 'Handmade',
+    episode: [e1, e2, e3, e4, e5, e6, e7],
+    description: "Handmade cookware and cuisine phenomena.",
+    image: require('../ThumbNails/handmade.jpg'),
+    thumbnail: require('../ThumbNails/handmadethumb.png')
+};
+
+export var handmade;
