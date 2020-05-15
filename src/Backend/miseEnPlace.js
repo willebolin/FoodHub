5ad0cc62d9f4ec2f19ae422bc1e71dcd99149932
AntoineLrt <56179864+AntoineLrt@users.noreply.github.com>
2020-05-14 22:38:04 -0700

    Create miseEnPlace.js

---
+++ src/Backend/miseEnPlace.js
@@ -0,0 +1,75 @@
+var e1 = {
+    ID: 'Yi2A1_NR24s',
+    series_title: "Mise en Place",
+    episode_title: 'How a Master Chef Created a Michelin-Starred Restaurant in a Mall Food Court',
+    episode_number: '1',
+    description: 'Michelin-starred Dialogue serves a seasonal 18-course tasting menu out of an 800-square-foot space inside a mall food court in Santa Monica. Executive chef and owner Dave Beran’s highly conceptual menu features dishes that symbolize the layers of the ground, hidden under leaves in winter, like a dish featuring pumpkin seeds covered in burnt parsley that simulates the twigs and seeds of a forest floor, or a black sesame and whipped yuzu course that resembles a spoonful of dirt. ',
+};
+
+var e2 = {
+    ID: 'NGbFtTYQpus',
+    series_title: "Mise en Place",
+    episode_title: 'How a Master Chef Built a Michelin-Starred Taiwanese Restaurant in a Strip Mall',
+    episode_number: '2',
+    description: 'At Michelin-starred Kato in LA, chef Jon Yao creates dishes inspired by flavors from Los Angeles and Taiwan. The tiny restaurant, located in a strip mall, prides itself on having only one refrigerator in the entire establishment, meaning the chefs there have to start fresh for their 600 dishes every day.',
+};
+
+var e3 = {
+    ID: 'z2qNEOnlxjA',
+    series_title: "Mise en Place",
+    episode_title: 'How a Master Chef Runs a Two-Michelin-Star Alsatian Restaurant in New York',
+    episode_number: '3',
+    description: 'At the two-Michelin-starred French institution Gabriel Kreuther Restaurant in NYC, chefs strive to make unique experiences for every guest by changing the $245 tasting menu every day and for every individual diner.',
+};
+
+
+var e4 = {
+    ID: '0xPwHR8_D2w',
+    series_title: "Mise en Place",
+    episode_title: 'How Chef Wolfgang Puck Serves 25,000 Dishes at The Oscars Every Year',
+    episode_number: '4',
+    description: 'We follow Wolfgang Puck and over 200 of the best chefs from his restaurants around the world to see how they have been catering the Oscars Governors Ball for the last 25 years. The crew prepares 25,000 dishes to serve 1,500 celebrities in the span of one hour. From the prep work, to firing every dish as soon as the last award is announced, we see what it takes to pull off the night flawlessly.',
+};
+
+var e5 = {
+    ID: 'X2jakwIVLbY',
+    series_title: "Mise en Place",
+    episode_title: 'How a Legendary Chef Runs One of the World's Most Iconic Restaurants',
+    episode_number: '5',
+    description: 'On this episode of Mise En Place, we follow chef Jean-Georges Vongerichten through the kitchen at Jean-Georges, New York to see how he and his team painstakingly create and perfect dishes like yellowfin tuna noodles, sea urchin crostini, egg toast with caviar, perfectly cooked squab, and more.',
+};
+
+var e6 = {
+    ID: 'N5SrDhe4bE0',
+    series_title: "Mise en Place",
+    episode_title: 'How a Master Chef Runs the Only Las Vegas Restaurant Awarded 3 Michelin Stars',
+    episode_number: '6',
+    description: 'Welcome to part two of Chefs of the Strip. Today's episode focuses on Christophe De Lellis, the chef behind the majestic Joël Robuchon restaurant in the MGM Grand hotel.',
+};
+
+var e7 = {
+    ID: 'YLWjn0TFH3U',
+    series_title: "Mise en Place",
+    episode_title: 'How a Master Chef Runs a 2 Michelin Star Nordic Restaurant in Brooklyn',
+    episode_number: '7',
+    description: 'On this episode of Mise En Place, we follow chef Fredrik Berselius as he and his team prepare for dinner service at two-Michelin-Starred Aska in Brooklyn, leaving no detail unscrutinized',
+};
+
+var e8 = {
+    ID: 'KJxbHfAxIuI',
+    series_title: "Mise en Place",
+    episode_title: 'How Chef Emma Bengtsson Runs a Two-Michelin-Starred Swedish Restaurant in NYC',
+    episode_number: '8',
+    description: 'On this episode of Mise En Place, chef Emma Bengtsson tells use how she went from pastry chef to head chef of Two-Michelin-Starred Aquavit, and how she makes her world famous bird's nest dessert.',
+};
+
+
+var miseEnPlace = {
+    title: 'Mise en Place',
+    episode: [e1, e2, e3, e4, e5, e6, e7, e8],
+    description: "Deep dive in the day-to-day of world famous chefs.",
+    image: require('../ThumbNails/miseEnPlace.jpg'),
+    thumbnail: require('../ThumbNails/miseEnPlacethumb.png')
+};
+
+export var miseEnPlace;
