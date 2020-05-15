ab8c64cf82b9e080b0ea1f9798d73d86495fbaf7
AntoineLrt <56179864+AntoineLrt@users.noreply.github.com>
2020-05-14 22:42:12 -0700

    Update reverseEngineering.js

--- src/Backend/reverseEngineering.js
+++ src/Backend/reverseEngineering.js
@@ -1,83 +1,82 @@
 var e1 = {
     ID: 'mfrqxqR4bJo',
     series_title: "Reverse Engineering",
-    episode_title: 'Recreating Leah Chase's Gumbo from Taste',
+    episode_title: 'Recreating Leah Chase\'s Gumbo from Taste',
     episode_number: '1',
-    description: 'We challenged resident Bon Appétit super taster Chris Morocco to recreate Queen of Creole Cuisine Leah Chase's gumbo recipe using every sense he has other than sight. Was he up to the challenge?',
+    description: 'We challenged resident Bon Appétit super taster Chris Morocco to recreate Queen of Creole Cuisine Leah Chase\'s gumbo recipe using every sense he has other than sight. Was he up to the challenge?',
 };
 
 var e2 = {
     ID: 'i3diNx1fV7E',
     series_title: "Reverse Engineering",
-    episode_title: 'Recreating Bobby Flay's Macaroni & Cheese Carbonara From Taste',
+    episode_title: 'Recreating Bobby Flay\'s Macaroni & Cheese Carbonara From Taste',
     episode_number: '2',
-    description: 'We challenged resident Bon Appétit super taster Chris Morocco to recreate Bobby Flay's macaroni & cheese carbonara recipe using every sense he has other than sight. Was he up to the challenge?',
+    description: 'We challenged resident Bon Appétit super taster Chris Morocco to recreate Bobby Flay\'s macaroni & cheese carbonara recipe using every sense he has other than sight. Was he up to the challenge?',
 };
 
 var e3 = {
     ID: 'Vsb0g1XUpCQ',
     series_title: "Reverse Engineering",
-    episode_title: 'Recreating Madhur Jaffrey's Chicken Tikka Masala From Taste',
+    episode_title: 'Recreating Madhur Jaffrey\'s Chicken Tikka Masala From Taste',
     episode_number: '3',
-    description: 'We challenged resident Bon Appétit super taster Chris Morocco to recreate Madhur Jaffrey's chicken tikka masala recipe using every sense he has other than sight. Was he up to the challenge? As a bonus, we're joined by the legend herself, Madhur, to review Chris's reverse engineered dish.',
+    description: 'We challenged resident Bon Appétit super taster Chris Morocco to recreate Madhur Jaffrey\'s chicken tikka masala recipe using every sense he has other than sight. Was he up to the challenge? As a bonus, we\'re joined by the legend herself, Madhur, to review Chris\'s reverse engineered dish.',
 };
 
 var e4 = {
     ID: 'qom3PQTLTr8',
     series_title: "Reverse Engineering",
-    episode_title: 'Recreating Roy Choi's Carne Asada Tacos From Taste',
+    episode_title: 'Recreating Roy Choi\'s Carne Asada Tacos From Taste',
     episode_number: '4',
-    description: 'We challenged resident Bon Appétit super taster Chris Morocco to recreate Roy Choi's carne asada Tacos recipe using every sense he has other than sight. Was he up to the challenge?',
+    description: 'We challenged resident Bon Appétit super taster Chris Morocco to recreate Roy Choi\'s carne asada Tacos recipe using every sense he has other than sight. Was he up to the challenge?',
 };
 
 var e5 = {
     ID: 'DKsxda--GVA',
     series_title: "Reverse Engineering",
-    episode_title: 'Recreating Paul Hollywood's Cornish Pasties from Taste',
+    episode_title: 'Recreating Paul Hollywood\'s Cornish Pasties from Taste',
     episode_number: '5',
-    description: 'We challenged resident Bon Appétit super taster Chris Morocco to recreate 'The Great British Bake Off' judge Paul Hollywood's Cornish Pasties recipe using every sense he has other than sight. Was he up to the challenge?',
+    description: 'We challenged resident Bon Appétit super taster Chris Morocco to recreate \'The Great British Bake Off\' judge Paul Hollywood\'s Cornish Pasties recipe using every sense he has other than sight. Was he up to the challenge?',
 };
 
 var e6 = {
     ID: '8wAgaNPRnbU',
     series_title: "Reverse Engineering",
-    episode_title: 'Recreating Snoop Dogg's Lobster Thermidor From Taste',
+    episode_title: 'Recreating Snoop Dogg\'s Lobster Thermidor From Taste',
     episode_number: '6',
-    description: 'What's the one thing that everybody consistently associates Snoop Dogg with? That's right, the classic French dish Lobster Thermidor. We challenged resident Bon Appétit super taster Chris Morocco to recreate Snoop's famous lobster recipe using every sense he has other than sight.',
+    description: 'What\'s the one thing that everybody consistently associates Snoop Dogg with? That\'s right, the classic French dish Lobster Thermidor. We challenged resident Bon Appétit super taster Chris Morocco to recreate Snoop\'s famous lobster recipe using every sense he has other than sight.',
 };
 
 
 var e7 = {
     ID: 'E3e20718ioY',
     series_title: "Reverse Engineering",
-    episode_title: 'Recreating Guy Fieri's Trash Can Nachos From Taste',
+    episode_title: 'Recreating Guy Fieri\'s Trash Can Nachos From Taste',
     episode_number: '7',
-    description: 'One dish. Two days. We challenged super taster Chris Morocco to blindly taste a dish made by Rick and then reverse engineer it as closely to the original as he can. The challenge this episode: Guy Fieri's Trash Can Nachos.',
+    description: 'One dish. Two days. We challenged super taster Chris Morocco to blindly taste a dish made by Rick and then reverse engineer it as closely to the original as he can. The challenge this episode: Guy Fieri\'s Trash Can Nachos.',
 };
 
 var e8 = {
     ID: 'EteEqlQgr9A',
     series_title: "Reverse Engineering",
-    episode_title: 'Recreating Maangchi's Dakbokkeumtang (Spicy Braised Chicken) From Taste',
+    episode_title: 'Recreating Maangchi\'s Dakbokkeumtang (Spicy Braised Chicken) From Taste',
     episode_number: '8',
-    description: 'One dish. Two days. We challenged super taster Chris Morocco to blindly taste a dish made by Christina and then reverse engineer it as closely to the original as he can. The challenge this episode: Maangchi's Dakbokkeumtang (Korean spicy braised chicken).',
+    description: 'One dish. Two days. We challenged super taster Chris Morocco to blindly taste a dish made by Christina and then reverse engineer it as closely to the original as he can. The challenge this episode: Maangchi\'s Dakbokkeumtang (Korean spicy braised chicken).',
 };
 
 var e9 = {
     ID: 'zlG_iUEybgM',
     series_title: "Reverse Engineering",
-    episode_title: 'Recreating Jamie Oliver's Insanity Burger From Taste',
+    episode_title: 'Recreating Jamie Oliver\'s Insanity Burger From Taste',
     episode_number: '9',
-    description: 'One dish. Two days. We challenged super taster Chris Morocco to blindly taste a dish made by Andy and then reverse engineer it as closely to the original as he can. The challenge this episode: Jamie Oliver's Insanity Burger.',
+    description: 'One dish. Two days. We challenged super taster Chris Morocco to blindly taste a dish made by Andy and then reverse engineer it as closely to the original as he can. The challenge this episode: Jamie Oliver\'s Insanity Burger.',
 };
 
 var e10 = {
     ID: '3eVMgUrEazA',
     series_title: "Reverse Engineering",
-    episode_title: 'Recreating Gordon Ramsay's Beef Wellington From Taste',
+    episode_title: 'Recreating Gordon Ramsay\'s Beef Wellington From Taste',
     episode_number: '10',
-    description: 'One dish. Two days. We challenged super taster Chris Morocco to blindly taste a dish made by Carla and then reverse engineer it as closely to the original as he can. The challenge this episode: Gordon Ramsay's Beef Wellington.
-P.S. We really just wanted to see Chris wear that sloth blindfold. Very worth it.',
+    description: 'One dish. Two days. We challenged super taster Chris Morocco to blindly taste a dish made by Carla and then reverse engineer it as closely to the original as he can. The challenge this episode: Gordon Ramsay\'s Beef Wellington. P.S. We really just wanted to see Chris wear that sloth blindfold. Very worth it.',
 };
 
 var reverseEngineering = {
